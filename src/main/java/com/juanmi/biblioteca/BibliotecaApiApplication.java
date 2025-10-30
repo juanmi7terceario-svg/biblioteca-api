aa0c3b85516a0dab1683bea723694ae75c37f0eb
Juan Miguel Sandoval <juanmi7terceranio-svg@users.noreply.github.com>
2025-10-30 13:15:28 -0600

    chore: proyecto base Spring Boot para sistema de biblioteca

---
+++ src/main/java/com/juanmi/biblioteca/BibliotecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.juanmi.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApiApplication.class, args);
+	}
+
+}
